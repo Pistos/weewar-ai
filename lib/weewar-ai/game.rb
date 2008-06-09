bd5a7db3ff9570e7298e81030300a74cbdb4c263
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-06-09 10:50:29 -0400

    Added examples to Game class documentation.

--- lib/weewar-ai/game.rb
+++ lib/weewar-ai/game.rb
@@ -15,6 +15,7 @@ module WeewarAI
     
     # Instantiate a new Game instance corresponding to the weewar game
     # with the given id number.
+    #   game = WeewarAI::Game.new( 132 )
     def initialize( id )
       @id = id.to_i
       refresh
@@ -27,6 +28,7 @@ module WeewarAI
     
     # Hits the weewar server for all the game state data as it sees it.
     # All internal variables are updated to match.
+    #   my_game.refresh
     def refresh
       xml = XmlSimple.xml_in(
         WeewarAI::API.get( "/gamestate/#{id}" ),
@@ -89,17 +91,20 @@ module WeewarAI
     #++
     
     # End turn in this game.
+    #   game.finish_turn
     def finish_turn
       send "<finishTurn/>"
     end
     alias finishTurn finish_turn
     
     # Surrender in this game.
+    #  game.surrender
     def surrender
       send "<surrender/>"
     end
     
     # Abandon this game.
+    #  game.abandon
     def abandon
       send "<abandon/>"
     end
@@ -109,6 +114,7 @@ module WeewarAI
     #++
     
     # The Player whose turn it is.
+    #   turn_taker = game.current_player
     def current_player
       @players.find { |p| p.current? }
     end
@@ -118,48 +124,49 @@ module WeewarAI
     #++
     
     # The Faction of the given player.
+    #   pistos_faction = game.faction_for_player 'Pistos'
     def faction_for_player( player_name )
       @factions.find { |f| f.player_name == player_name }
     end
     
     # Your AI's Faction in this game.
+    #   me = my = i = game.my_faction
+    #   puts "My name is #{my.player_name}."
+    #   if i.can_afford? :htank
+    #     my_base.build :htank
+    #   end
     def my_faction
       faction_for_player WeewarAI::API.username
     end
     
-    # An Array of the Units not belonging to the given faction.
-    def units_not_of( faction )
-      @units.find_all { |u| u.faction != faction }
-    end
-    
     # An Array of the Units not belonging to your AI.
+    #   bad_guys = game.enemy_units
     def enemy_units
-      units_not_of my_faction
+      @units.find_all { |u| u.faction != my_faction }
     end
     
     # An Array of the base Hexes for this game.
+    #   bases = game.bases
     def bases
       @map.bases
     end
     
     # An Array of the base Hexes owned by the given faction.
+    #   their_bases = game.bases enemy_faction
     def bases_of( faction )
       @map.bases.find_all { |b| b.faction == faction }
     end
     
     # Your AI's bases in this game.
+    #   good_bases = game.my_bases
     def my_bases
       bases_of my_faction
     end
     
-    # An Array of the base Hex es which are not owned by the given faction.
-    def bases_not_of( faction )
-      @map.bases.find_all { |b| b.faction != faction }
-    end
-    
     # An Array of bases not owned by your AI (including neutral bases).
+    #   capturable_bases = game.enemy_bases
     def enemy_bases
-      bases_not_of my_faction
+      @map.bases.find_all { |b| b.faction != my_faction }
     end
         
   end
